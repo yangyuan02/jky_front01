27d2467d21d3040a72add7faeff8b43c919a83ba
yangyuan <1367511704@qq.com>
2018-03-27 13:26:52 +0800

    登录页路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+const Login = r => require.ensure([], () => r(require('@/views/login')), 'Login')
+
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      component: Login
     }
   ]
 })
