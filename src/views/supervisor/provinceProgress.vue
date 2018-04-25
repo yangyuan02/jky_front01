36cddf2aaf2e5fb98a0e5622c75b7cbe824f4241
yangyuan <1367511704@qq.com>
2018-04-25 21:02:09 +0800

    修改督导用户

---
+++ src/views/supervisor/provinceProgress.vue
@@ -0,0 +1,106 @@
+<template>
+    <div class="department">
+        <div class="sup_right">
+            <div class="depart_head">
+                省自评进度情况
+            </div>
+            <div class="depart_con">
+                <ul>
+                    <li class="active" @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li class="active" @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li class="active" @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li class="active" @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li class="active" @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li class="active" @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li class="active" @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                    <li @click="goDetail"><span>北京市</span><span>10/10</span></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+
+
+export default {
+    methods:{
+        getDetail(){
+            this.$ajax.post("/api/admin/assessments/progress").then((res)=>{
+                console.log(res)
+            })
+        },
+        goDetail(){//跳转详情
+            this.$router.push('/home/department/detail')
+        }
+    },
+    mounted(){
+        this.getDetail()
+    }
+}
+</script>
+
+<style>
+    .department{
+        display: flex;
+    }
+    .sup_right{
+        width: 100%;
+        height: 600px;
+        background: #fff;
+        box-shadow: 1px 1px 8px #ccc;
+        padding: 20px;
+    }
+    .sup_right .depar_head{
+        width: 100%;
+        height: 38px;
+        line-height: 38px;
+        text-align: center;
+        background: #a4c9e9;
+        color: #fff;
+    }
+    .sup_right .back{
+        color: #419bf6
+    }
+    .sup_right .back a{
+        color: #419bf6
+    }
+    .sup_right .depart_head{
+        height: 38px;
+        line-height: 38px;
+        text-align: center;
+        background: #a4c9e9;
+        color: #fff;
+    }
+    .sup_right .depart_con ul{
+        display: flex;
+        justify-content:space-between;
+        flex-wrap: wrap;
+    }
+    .sup_right .depart_con ul::after{
+         content: "";
+         flex: auto;
+    }
+    .sup_right .depart_con ul li{
+        padding: 10px 5px;
+        color: #00a0e9;
+        border: 1px solid #00a0e9;
+        width: 120px;
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        margin-top: 14px;
+        /* margin-right: 10px; */
+    }
+    .sup_right .depart_con ul li:nth-child(7n){
+        margin-right: 0px;
+    }
+    .sup_right .depart_con ul li.active{
+        color: #fff;
+        border:1px solid #4adedd;
+        background: #4adedd;
+    }
+</style>
+
